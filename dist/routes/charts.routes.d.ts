d698b14c8d2a4a2ab8eaf833eab4e9450bee537f
Vaibhav Satam <vaibhav.satam@accionlabs.com>
2026-02-19 04:41:34 +0530

    added metadata

--- dist/routes/charts.routes.d.ts
+++
@@ -1,4 +0,0 @@
-import { Router } from 'express';
-import { ChartService } from '../services/ChartService';
-export declare function createChartsRouter(chartService: ChartService): Router;
-//# sourceMappingURL=charts.routes.d.ts.map
--- dist/routes/platforms.routes.d.ts
+++
@@ -1,4 +0,0 @@
-import { Router } from 'express';
-import { ChartService } from '../services/ChartService';
-export declare function createPlatformsRouter(chartService: ChartService): Router;
-//# sourceMappingURL=platforms.routes.d.ts.map
